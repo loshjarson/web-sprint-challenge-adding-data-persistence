85d0ffc7b433ba99e2b6e7c7f38aa694b28f256c
unknown <jlarson129@outlook.com>
2021-12-26 17:39:36 -0500

    project model and router

--- api/project/model.js
+++ api/project/model.js
@@ -1,1 +1,46 @@
-// build your `Project` model here
+const db = require('../../data/dbConfig');
+
+const convertIntegersToBoolean = (project) => {
+
+    project.project_completed = (project.project_completed ? true : false)
+    return project
+}
+
+const convertBooleansToInteger = (project) => {
+
+    project.project_completed = (project.project_completed ? 1 : 0)
+    return project
+
+}
+
+
+function getAll() {
+  return db('projects')
+    .then(projects => {
+        projects.forEach(project =>
+        convertIntegersToBoolean(project)
+        )
+        return projects
+    })
+}
+
+function findById(project_id) {
+  return db('projects')
+    .where({ project_id }).first()
+    .then(project => {
+        return convertIntegersToBoolean(project)
+    })
+}
+
+function add(project) {
+    const newProject = convertBooleansToInteger(project)
+    return db("projects")
+        .insert(newProject, 'project_id')
+        .then(([project_id]) => findById(project_id));
+}
+
+module.exports = {
+  add,
+  getAll,
+  findById,
+};
--- api/project/router.js
+++ api/project/router.js
@@ -1,1 +1,24 @@
-// build your `/api/projects` router here
+const express = require('express');
+const Projects = require('./model');
+const router = express.Router();
+
+router.get('/', async (req ,res, next) => {
+    try {
+        const projects = await Projects.getAll()
+        res.status(200).json(projects)
+    } catch(err) {
+        next(err)
+    }
+})
+
+router.post('/', async (req, res, next) => {
+    const project = req.body
+    try {
+        const newProject = await Projects.add(project)
+        res.status(201).json(newProject)
+    } catch(err) {
+        next(err)
+    }
+})
+
+module.exports = router;
