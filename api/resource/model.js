c49c4ba691321feba3acaf308a54045bc23c9f5c
unknown <jlarson129@outlook.com>
2021-12-26 17:52:30 -0500

    resource model and router

--- api/resource/model.js
+++ api/resource/model.js
@@ -1,1 +1,23 @@
-// build your `Resource` model here
+const db = require('../../data/dbConfig');
+
+function getAll() {
+  return db('resources');
+}
+
+function findById(resource_id) {
+  return db('resources')
+    .where({ resource_id }).first()
+}
+
+function add(resource) {
+  return db("resources")
+    .insert(resource, 'resource_id')
+    .then(([resource_id]) => findById(resource_id));
+}
+
+
+module.exports = {
+  getAll,
+  add,
+  findById,
+}
