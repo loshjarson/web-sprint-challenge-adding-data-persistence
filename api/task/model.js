02a38ea7b2e815a27d78299f1b6db2e8819d53fb
unknown <jlarson129@outlook.com>
2021-12-26 18:15:57 -0500

    task model and router

--- api/task/model.js
+++ api/task/model.js
@@ -1,1 +1,47 @@
-// build your `Task` model here
+const db = require("../../data/dbConfig");
+
+const convertIntegersToBoolean = (task) => {
+
+    task.task_completed = (task.task_completed ? true : false)
+    return task
+}
+
+const convertBooleansToInteger = (task) => {
+
+    task.task_completed = (task.task_completed ? 1 : 0)
+    return task
+
+}
+
+function getAll() {
+    return db('tasks as t')
+    .leftJoin("projects as p", "t.project_id", "p.project_id")
+    .select("t.task_description", "t.task_notes", "t.task_completed", "p.project_name", "p.project_description")
+    .then(tasks => {
+        tasks.forEach(task =>
+        convertIntegersToBoolean(task)
+        )
+        return tasks
+    })
+}
+
+function findById(task_id) {
+    return db('tasks')
+        .where({ task_id }).first()
+        .then(task => {task
+            return convertIntegersToBoolean(task)
+        })
+}
+
+function add(task) {
+    const newTask = convertBooleansToInteger(task)
+    return db("tasks")
+    .insert(newTask, 'task_id')
+    .then(([task_id]) => findById(task_id));
+}
+
+module.exports = {
+  getAll,
+  add,
+  findById,
+};
--- api/task/router.js
+++ api/task/router.js
@@ -1,1 +1,27 @@
-// build your `/api/tasks` router here
+const express = require('express');
+const Tasks = require('./model');
+const router = express.Router();
+
+router.get('/', async (req, res, next) => {
+    try {
+        const tasks = await Tasks.getAll() 
+        res.status(200).json(tasks)
+      } catch (err) {
+        next(err)
+      }
+});
+
+router.post('/', async (req, res, next) => {
+  const taskData = req.body;
+
+  try {
+      const postedTask = await Tasks.add(taskData)
+      res.status(201).json(postedTask);
+    } catch (err) {
+      next(err)
+    }
+});
+
+
+
+module.exports = router;
